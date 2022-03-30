a618104fb6e63bc9fda7c9c83f31c3a1666634e3
Szymon Bednorz <dsonyyy@gmail.com>
2022-03-30 14:51:57 +0200

    insertion_sort

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,10 @@
+
+def insertion_sort(T):
+    for i in range(1, len(T)):
+        v = T[i]
+        j = i
+        while j > 0 and T[j - 1] > v:
+            T[j] = T[j - 1]
+            j -= 1
+        T[j] = v
+    return T
