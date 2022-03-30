bde76c54f826006e253a9ab2ed558d5d8cdd265e
Szymon Bednorz <dsonyyy@gmail.com>
2022-03-30 14:52:40 +0200

    simple testing framework for sorting

---
+++ sorting/testing.py
@@ -0,0 +1,25 @@
+from random import shuffle
+from insertion_sort import insertion_sort
+from heap_sort import heap_sort
+from merge_sort import merge_sort
+
+FN = (
+    insertion_sort,
+    heap_sort,
+    merge_sort,
+)
+
+for fn in FN:
+    print(fn.__name__ + ": ", end="")
+    for _ in range(100):
+        T = [i for i in range(100)]
+        shuffle(T)
+        A = sorted(T)
+        B = insertion_sort(T)
+        if A != B:
+            print("ERR!")
+            print("in: ", T)
+            print("out:", B)
+            break
+    else:
+        print("OK")
