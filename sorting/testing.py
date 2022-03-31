a7345cb4ccb43bff4000092b5834a853cb2f7b17
Szymon Bednorz <dsonyyy@gmail.com>
2022-03-31 12:57:24 +0200

    radix_sort

--- sorting/testing.py
+++ sorting/testing.py
@@ -3,12 +3,14 @@ from insertion_sort import insertion_sort
 from heap_sort import heap_sort
 from merge_sort import merge_sort
 from quick_sort import quick_sort
+from radix_sort import radix_sort
 
 FN = (
     insertion_sort,
     heap_sort,
     merge_sort,
     quick_sort,
+    radix_sort,
 )
 
 for fn in FN:
